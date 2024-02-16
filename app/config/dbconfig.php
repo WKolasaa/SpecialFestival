4bf19dae399a176096424a78800cb1a3dccea598
Slava <rastenie0102@gmail.com>
2024-02-16 13:58:25 +0100

    dbconfig changes

--- app/config/dbconfig.php
+++ app/config/dbconfig.php
@@ -1,7 +1,6 @@
 <?php
 $type = "mysql"; // This is the database type
-$servername = "mysql"; // This is the service name of the MySQL container as defined in Docker Compose
-$username = "root"; // This is the MySQL user specified in the environment variables
-$password = "secret123"; // This is the MySQL password specified in the environment variables
-$database = "HaarlemFestivaldb"; // This is the MySQL database specified in the environment variables
-
+$servername = "sql11.freemysqlhosting.net"; // Remote SQL server hostname
+$username = "sql11684523"; // Remote SQL server username
+$password = "1JgLceYy64"; // Remote SQL server password
+$database = "sql11684523"; // Remote SQL server database namev
